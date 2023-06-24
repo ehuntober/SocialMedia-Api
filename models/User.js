1d89bbfb5670e1ba686a263a96a7c6bfc8bf2014
ehuntober <ehuntercrypto@gmail.com>
2023-06-24 12:53:09 +0000

    created user data on User model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,24 @@
 const mongoose = require('mongoose');
 
 
-const
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 20,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 50,
+        unique: true
+    },
+    profilePicture:{
+        type: String,
+        required: true,
+        min: 6
+    },
+
+})
