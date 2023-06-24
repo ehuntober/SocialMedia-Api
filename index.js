2f4f8e3fddae83caf85d77c3e4779c0f1a7b1893
ehuntober <ehuntercrypto@gmail.com>
2023-06-24 12:23:32 +0000

    file modified

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv');
+
+
+const app = express();
+dotenv.config()
+
+app.use(cors());
+app.use(express.json())
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT,()=>{
+    console.log('Express server listening on port %s', PORT);
+})
