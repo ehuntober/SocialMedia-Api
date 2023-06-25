b94b851badfc98fdf60298d615e3c75b8c5747c3
ehuntober <ehuntercrypto@gmail.com>
2023-06-25 10:41:41 +0000

    errors fiexed

--- routes/users.js
+++ routes/users.js
@@ -45,12 +45,45 @@ router.put("/:id/follow", async(req,res)=>{
         }
 
         catch(error){
-            res.status(403).json("You are already fllowed  this user")
+            res.status(500).json(error)
         }
 
     }else{
         res.status(403).json("You can't follow yourself")
     }
 })
+
+
+// Unfollow user
+
+router.put("/:id/unfollow", async(req,res)=>{
+
+    if(req.body.userId != req.params.id){
+        try{
+
+    const user = await User.findById(req.params.id)
+    const currentUser = await User.findById(req.body.userId)
+    if(user.followers.includes(req.body.userId)){
+        await user.updateOne({$push: {followers: req.body.userId}})
+        await currentUser.updateOne({$push: { $followings: req.params.id}})
+        res.status(200).json("user has been unfollowed")
+         }
+         else{
+            res.status(403).json("You don't follow this user")
+         }
+        }
+
+        catch(error){
+            res.status(500).json(error)
+        }
+
+    }else{
+        res.status(403).json("You can't follow yourself")
+    }
+})
+
+
+
+
 
 
