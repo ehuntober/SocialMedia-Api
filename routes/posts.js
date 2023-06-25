de1ccb9df0be2b4c991e8806ac7b2c1376c8f6a7
ehuntober <ehuntercrypto@gmail.com>
2023-06-25 08:02:51 +0000

    post updated

--- routes/posts.js
+++ routes/posts.js
@@ -21,25 +21,20 @@ router.post("/", async(req,res)=>{
 })
 
 // Update existing Post
-router.put("/id", async(req,res)=>{
-    try{
-        const post = await Post.findOne(req.params.id);
-        if(post.userId === req.body.userId){
-            await post.updateOne({ $set: req.body})
-            res.status(200).json("Your post has been updated!!!")
-        } else{
-            res.status(403).json("You can only update your post !")
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (post.userId === req.body.userId) {
+            await post.updateOne({ $set: req.body });
+            res.status(200).json("Your post has been updated!!!");
+        } else {
+            res.status(403).json("You can only update your post!");
         }
-        res.status(200).json({
-            "message": "Post created successfully",
-            "data": savedPost
-        })
-
+    } catch (error) {
+        res.status(500).json(error);
     }
-    catch(error){
-        res.status(500).json(error)
+});
 
-    }
-})
 
 module.exports = router
