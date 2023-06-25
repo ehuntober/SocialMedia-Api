a6522a96ee0c05ed0c2bb17c549d4f3e6464f863
ehuntober <ehuntercrypto@gmail.com>
2023-06-25 08:19:00 +0000

    new file added

--- routes/posts.js
+++ routes/posts.js
@@ -26,10 +26,47 @@ router.put("/:id", async (req, res) => {
         const post = await Post.findById(req.params.id);
 
         if (post.userId === req.body.userId) {
-            await post.updateOne({ $set: req.body });
-            res.status(200).json("Your post has been updated!!!");
+            await post.updateOne({ $set: req.body }, {new: true});
+            res.status(200).json({
+                "message":"Your post has been updated!!!",
+                "data": post})
         } else {
             res.status(403).json("You can only update your post!");
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+// delete post
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (post.userId === req.body.userId) {
+            await post.deleteOne({ $set: req.body }, {new: true});
+            res.status(200).json("Your post have been deleted")
+        } else {
+            res.status(403).json("You can only delete your post!");
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+// get all posts
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (post.userId === req.body.userId) {
+            await post.deleteOne({ $set: req.body }, {new: true});
+            res.status(200).json("Your post have been deleted")
+        } else {
+            res.status(403).json("You can only delete your post!");
         }
     } catch (error) {
         res.status(500).json(error);
